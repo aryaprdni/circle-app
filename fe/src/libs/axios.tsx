4f979d27d954de488157aaa36edf20b88035ff63
arya <aryaperdanairawan473@gmail.comgit config --global user.name aryaprdni;git config --global user.email aryaperdanairawan473@gmail.com>
2024-05-15 00:23:47 +0700

    update env port n api railways

--- fe/src/libs/axios.tsx
+++ fe/src/libs/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://circle-app-server.up.railway.app/api/v1",
 });
 
 export function setAuthToken(token: string | null) {
